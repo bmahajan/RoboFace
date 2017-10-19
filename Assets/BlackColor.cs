ae0ec07e4ed10ce5a74515b5736330ec8eb63fa1
bmahajan <bm.bonnymahajan@gmail.com>
2017-10-19 15:53:24 -0500

    Files added

---
+++ Assets/BlackColor.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlackColor : MonoBehaviour {
+
+	public float speed = 5f;
+	public float maxDistance = 1f;
+	public Camera mainCamera ;
+	private Vector3 _origin;
+	// Use this for initialization
+	void Start () {
+		mainCamera = Camera.main;
+		_origin = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Get the mouse space in world space
+		var mouseWorldCoordinates = mainCamera.ScreenPointToRay(Input.mousePosition).origin;
+		//get Vector pointing from initial position to the target. Vector cant be longer than
+		//max distance
+		var originToMouse = mouseWorldCoordinates - _origin;
+		originToMouse = Vector3.ClampMagnitude (originToMouse, maxDistance);
+
+		//Linearly interpolate from current to mouse's position
+		transform.position = Vector3.Lerp(transform.position, _origin + originToMouse, speed * Time.deltaTime);
+		Debug.Log (transform.position);
+
+		//Link to move it for fixed tim
+		//http://answers.unity3d.com/questions/34884/move-a-gameobject-for-certain-amount-of-time-and-t.html
+	}
+}
---
+++ Assets/Sound.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sound : MonoBehaviour {
+
+	float[] samples;
+	public AudioSource audioSource;
+	// Use this for initialization
+	void Start () {
+		audioSource = GetComponent<AudioSource> ();
+		samples = new float[audioSource.clip.samples * audioSource.clip.channels];
+		//store sample data from clip in this array
+		audioSource.clip.GetData(samples, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		//in every frame get the current sample of the clip from the samples array
+		float currentSample = samples[audioSource.timeSamples * audioSource.clip.channels];
+		if (currentSample > 0.05) {
+			transform.localScale = new Vector3 (1.5F, 2.2F * currentSample, 0.5F);
+		} else {
+			transform.localScale = new Vector3 (1.5F, 0.1F, 0.5F);
+		}
+		
+	}
+}
