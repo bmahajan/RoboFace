056920dbf1894d3c943cc72c775642d7155b0ce2
Brigette Krause <bkrause44@gmail.com>
2017-11-19 15:59:21 -0600

    Brigette LeftRightEye Trial Commit

--- Assets/BlackColor.cs
+++ Assets/BlackColor.cs
@@ -4,30 +4,101 @@ using UnityEngine;
 
 public class BlackColor : MonoBehaviour {
 
-	public float speed = 5f;
-	public float maxDistance = 1f;
+	public float speed = .5f;
+	public float maxDistance = 3f;
 	public Camera mainCamera ;
 	private Vector3 _origin;
+	public bool move = true;
+	public int count = 0;
+	public float startTime;
+
 	// Use this for initialization
 	void Start () {
 		mainCamera = Camera.main;
 		_origin = transform.position;
+		startTime = Time.time;
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		Vector3 leftPosition = new Vector3 (transform.position.x+.3f,transform.position.y,transform.position.z);
+		Vector3 rightPosition = new Vector3 (transform.position.x-.6f,transform.position.y,transform.position.z);
+		if (count >1) {
+			leftPosition = new Vector3 (transform.position.x+.6f,transform.position.y,transform.position.z);
+		}
+	
+		/*if (count < 20) {
+			if (move) {
+				transform.position = Vector3.Lerp (transform.position, leftPosition, transform.position.z);
+				//transform.position = Vector3.MoveTowards (transform.position, leftPosition, speed);
+				move = false;
+			} else {
+				transform.position = Vector3.Lerp (transform.position, rightPosition, speed);
+				//transform.position = Vector3.MoveTowards (transform.position, rightPosition, speed);
+				move = true;
+
+			}
+			count++;
+			
+		} else {
+			transform.position = _origin;
+		
+		
+		}*/
+
+
+
+
+		//transform.position.x = Mathf.PingPong(Time.time,2.0f) - 1.0f;
+		if (count <200) {
+			count++;
+			if (move) {
+				var temp = new Vector3 (Mathf.PingPong (Time.time, 1.0f) + 1.0f, transform.position.y, transform.position.z);
+				transform.position = Vector3.ClampMagnitude (temp, maxDistance);
+			} else {
+				var temp2 = new Vector3 (Mathf.PingPong (Time.time, 1.0f) - 0.5f, transform.position.y, transform.position.z);
+				transform.position = Vector3.ClampMagnitude (temp2, maxDistance);
+
+			}
+					
+		} else if (count == 800) {
+			count = 0;
+			//transform.position = _origin;
+		} else {
+			transform.position = _origin;
+
+			count++;		
+		}
+
+
+
+
+			
+		
+		//if (transform.position.x > 0) {
+		//	for (float i = transform.position.x; i < 1.75f; i++) {
+		//		transform.Translate (Vector3.right * Time.deltaTime);
+		//	}
+		
+		//} 
+
+
 		//Get the mouse space in world space
-		var mouseWorldCoordinates = mainCamera.ScreenPointToRay(Input.mousePosition).origin;
+		//var mouseWorldCoordinates = mainCamera.ScreenPointToRay(Input.mousePosition).origin;
 		//get Vector pointing from initial position to the target. Vector cant be longer than
 		//max distance
-		var originToMouse = mouseWorldCoordinates - _origin;
-		originToMouse = Vector3.ClampMagnitude (originToMouse, maxDistance);
+		//var originToMouse = mouseWorldCoordinates - _origin;
+		//originToMouse = Vector3.ClampMagnitude (originToMouse, maxDistance);
+
 
 		//Linearly interpolate from current to mouse's position
-		transform.position = Vector3.Lerp(transform.position, _origin + originToMouse, speed * Time.deltaTime);
-		Debug.Log (transform.position);
+		//transform.position = Vector3.Lerp(transform.position, _origin + originToMouse, speed * Time.deltaTime);
+		//Debug.Log (transform.position);
 
 		//Link to move it for fixed tim
 		//http://answers.unity3d.com/questions/34884/move-a-gameobject-for-certain-amount-of-time-and-t.html
+
+
 	}
 }
---
+++ Assets/buttonScript.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class buttonScript : MonoBehaviour {
+	//, IPointerDownHandler
+	private bool[] mousePressed = { false, false, false };
+	public int count = 0;
+	float rate = 200.0f;
+	//List<MonoBehaviour> delayedUpdatedStuff = new List<MonoBehaviour>();
+
+	void Start(){
+		//Debug.Log ("Start");
+		InvokeRepeating ("SlowUpdate", 0.0f,rate);
+	}
+
+	void SlowUpdate() {
+		//Debug.Log ("Updated");
+
+		if (Input.GetMouseButtonDown (0))
+			Debug.Log ("clicked");
+			//clickFunction();
+	}
+
+
+	private void clickFunction()
+	{
+
+		Debug.Log ("in click function\n");
+		if (mousePressed [0] || mousePressed [1] || mousePressed [2])
+		{
+			Debug.Log ("clicked");
+			mousePressed[0] = false;
+			mousePressed [1] = false;
+			mousePressed [2] = false;
+		}
+
+	}
+		
+
+	void Update()
+	{
+		//Debug.Log ("update called");
+
+			mousePressed [0] = Input.GetMouseButtonDown (0);
+			mousePressed [1] = Input.GetMouseButtonDown (1);
+			mousePressed [2] = Input.GetMouseButtonDown (2);
+
+
+	
+		clickFunction ();
+	}
+
+
+
+}
