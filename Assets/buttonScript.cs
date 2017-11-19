18edb15a97e1a37422b2f34f83abc39a6ab53941
Anna <annacwang8@gmail.com>
2017-11-19 14:44:03 -0600

    First commit

---
+++ Assets/buttonScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class buttonScript : MonoBehaviour, IPointerDownHandler {
+
+
+	void Start()
+	{
+		addPhysicsRaycaster();
+	}
+
+	void addPhysicsRaycaster()
+	{
+		PhysicsRaycaster physicsRaycaster = GameObject.FindObjectOfType<PhysicsRaycaster>();
+		if (physicsRaycaster == null)
+		{
+			Camera.main.gameObject.AddComponent<PhysicsRaycaster>();
+			Debug.Log ("Added ray Caster");
+		}
+	}
+	void Update()
+	{
+		Debug.Log ("update");
+	}
+	public void OnPointerDown(PointerEventData eventData)
+	{
+		Debug.Log ("Clciked " + eventData.pointerCurrentRaycast.gameObject.name);
+	}
+}
